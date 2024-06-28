ca21468ab771ed5ff7c0cd24de0fe86532501d0b
Fadwaepsi <fadwa.bayoudh@ecoles-epsi.net>
2024-06-28 05:14:33 +0200

    📦 NEW: Rajout d'autre modification sur locust file

--- app/main.py
+++ app/main.py
@@ -1,5 +1,4 @@
-from api.v1.Fonction_Astro import get_sunrise_sunset
-
+from api.v1.Fonction_Astro import get_sunrise_sunset, get_moon_phases
 from fastapi import FastAPI, HTTPException
 
 app = FastAPI()
@@ -7,11 +6,22 @@ app = FastAPI()
 api_key = "4c57c92aa17e4b82857172100241506"
 
 @app.get("/heures/{city}")
-def read_root(city: str):
+def read_sunrise_sunset(city: str):
     try:
         sunrise, sunset = get_sunrise_sunset(city, api_key)
         if sunrise and sunset:
-            return {f"L'heure de lever et coucher de soleil à":city, "est": sunrise, "et":sunset}
+            return {f"L'heure de lever et coucher de soleil à": city, "est": sunrise, "et": sunset}
+        else:
+            raise HTTPException(status_code=404, detail="Data not found")
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+@app.get("/phases_lune/{city}")
+def read_moon_phases(city: str):
+    try:
+        moon_phase = get_moon_phases(city, api_key)
+        if moon_phase:
+            return {f"La phase de la lune à": city, "est": moon_phase}
         else:
             raise HTTPException(status_code=404, detail="Data not found")
     except Exception as e:
